42bd4eced4b695f7a144f23e479a35fcb2d1cddf
William Desportes <williamdes@wdes.fr>
2021-04-24 15:21:54 +0200

    Handle SKIP_DEPLOY to use the container as a workspace and handle permission issues

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ The container for gh-deployer
 
 You can also specify:
 
+- a memory limit `-e MEMORY_LIMIT=128M`
 - a SHA `-e GIT_SHA=8669c00fff7c08ac58917e3fcc7f87e28f2052c5`
 - a network `--network mynetwork` (see: `docker network ls`)
 - a config `-e PMA_CONFIG="$(cat config.inc.php | base64 -w0)"` (if not provided the one built into the container will be used)
@@ -25,7 +26,23 @@ docker run --rm --name myDeployedContainer1 \
         ghcr.io/sudo-bot/gh-deployer-container/phpmyadmin
 ```
 
+### Skip deploy and use your workspace folder
+
+NB: you need to keep the user root to make nginx bind port work
+
+```sh
+docker run --rm --name myStaticContainer1 \
+    --user "0:$(id -g)" \
+    -p 8085:80 \
+    -v $PWD:/home/www/phpMyAdmin \
+    -e SKIP_DEPLOY=yes \
+    -e MEMORY_LIMIT=128M \
+        ghcr.io/sudo-bot/gh-deployer-container/phpmyadmin
+```
+
 ### Stop the container
+
+If you use `-it` you will be able to kill the container with a CTRL-C
 
 /!\ This will auto destroy the containers contents (`--rm`)
 Run: `docker stop myDeployedContainer1`
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,67 +2,99 @@
 
 set -e
 
-echo 'Setup ENVs'
-
-# Defined by the deployer
-printf "env[RANDOM_STRING] = '%s';\nenv[DEPLOY_SHA] = '%s';\nenv[DEPLOY_BRANCH] = '%s';\n" \
-    "$RANDOM_STRING" "${GIT_SHA:--}" "${GIT_BRANCH:--}" >> /etc/php7/php-fpm.d/www.conf
-
-if [ -n "$REF_DIRECTORY" ]; then
-    REF_DIR="--reference $REF_DIRECTORY"
-else
-    REF_DIR=""
-fi
-
 WWW_ROOT='/home/www/phpMyAdmin'
 
-set -x
-echo 'Cloning ...'
+setupEnvs() {
+    echo 'Setup ENVs'
 
-if [ ! -d "${WWW_ROOT}/.git" ]; then
-    echo "Cloning dir !"
+    # Defined by the deployer
+    printf "env[RANDOM_STRING] = '%s';\nenv[DEPLOY_SHA] = '%s';\nenv[DEPLOY_BRANCH] = '%s';\n" \
+        "$RANDOM_STRING" "${GIT_SHA:--}" "${GIT_BRANCH:--}" >> /etc/php7/php-fpm.d/www.conf
 
-    git clone $REF_DIR --depth 1 --branch "${GIT_BRANCH}" "$GIT_URL" "${WWW_ROOT}"
-    cd "${WWW_ROOT}"
-    if [ ! -z "${GIT_SHA}" ]; then
-        git reset --hard "${GIT_SHA}"
+    if [ -n "$REF_DIRECTORY" ]; then
+        REF_DIR="--reference $REF_DIRECTORY"
     else
-        printf 'GIT_SHA ENV is empty, skipping.'
+        REF_DIR=""
     fi
-    cd - > /dev/null
+}
+
+doClone() {
+    echo 'Cloning ...'
+
+    if [ ! -d "${WWW_ROOT}/.git" ]; then
+        echo "Cloning dir !"
+
+        git clone $REF_DIR --depth 1 --branch "${GIT_BRANCH}" "$GIT_URL" "${WWW_ROOT}"
+        cd "${WWW_ROOT}"
+        if [ ! -z "${GIT_SHA}" ]; then
+            git reset --hard "${GIT_SHA}"
+        else
+            printf 'GIT_SHA ENV is empty, skipping.'
+        fi
+        cd - > /dev/null
+    else
+        echo "Repo exists at ${WWW_ROOT}"
+    fi
+}
+
+resetToSha() {
+    if [ -z "${GIT_SHA}" ]; then
+        echo 'Using git pull'
+        cd ${WWW_ROOT}
+        git checkout "${GIT_BRANCH}"
+        git pull --ff-only
+        cd - > /dev/null
+    fi
+}
+
+makeTemp() {
+    if [ ! -d "${WWW_ROOT}/tmp" ]; then
+        echo "Creating temp dir !"
+        mkdir "${WWW_ROOT}/tmp"
+        chmod 777 "${WWW_ROOT}/tmp"
+    else
+        echo "Temp dir exists at ${WWW_ROOT}"
+    fi
+}
+
+saveConfig() {
+    if [ -n "$PMA_CONFIG" ]; then
+        echo "$PMA_CONFIG" | base64 -d > "${WWW_ROOT}/config.inc.php"
+    else
+        echo 'No PMA_CONFIG ENV found';
+        cp /config.inc.php "${WWW_ROOT}/config.inc.php"
+    fi
+}
+
+launchDeploy() {
+    echo 'Launch final deploy'
+    /deploy-branch.sh "${WWW_ROOT}" "$GIT_BRANCH" "$GIT_SHA"
+}
+
+injectMemoryLimit() {
+    if [ -n "${MEMORY_LIMIT}" ]; then
+        # Remove any previous config
+        # Sed -i is not possible because temp file permissions and redirect output does not work either
+        printf "$(sed '/memory_limit/d' /etc/php7/php-fpm.d/www.conf)" > /etc/php7/php-fpm.d/www.conf
+        printf '\nphp_admin_value[memory_limit] = %s\n' "${MEMORY_LIMIT}" >> /etc/php7/php-fpm.d/www.conf
+        echo 'Injected MEMORY_LIMIT config'
+    fi
+}
+
+if [ -z "${SKIP_DEPLOY}" ]; then
+    setupEnvs
+    doClone
+    resetToSha
+    makeTemp
+    saveConfig
+    launchDeploy
+    injectMemoryLimit
 else
-    echo "Repo exists at ${WWW_ROOT}"
+    injectMemoryLimit
 fi
-
-if [ -z "${GIT_SHA}" ]; then
-    echo 'Using git pull'
-    cd ${WWW_ROOT}
-    git checkout "${GIT_BRANCH}"
-    git pull --ff-only
-    cd - > /dev/null
-fi
-
-if [ ! -d "${WWW_ROOT}/tmp" ]; then
-    echo "Creating temp dir !"
-    mkdir "${WWW_ROOT}/tmp"
-    chmod 777 "${WWW_ROOT}/tmp"
-else
-    echo "Temp dir exists at ${WWW_ROOT}"
-fi
-
-if [ -n "$PMA_CONFIG" ]; then
-    echo "$PMA_CONFIG" | base64 -d > "${WWW_ROOT}/config.inc.php"
-else
-    echo 'No PMA_CONFIG ENV found';
-    cp /config.inc.php "${WWW_ROOT}/config.inc.php"
-fi
-
-echo 'Launch final deploy'
-
-/deploy-branch.sh "${WWW_ROOT}" "$GIT_BRANCH" "$GIT_SHA"
 
 echo 'Starting the services'
 
 php-fpm7
 
-nginx -g 'pid /tmp/nginx.pid; daemon off;'
+nginx
